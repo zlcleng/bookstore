882c3e342ce4a68b1f5cbddd50f7256d82327626
zhenglc <zlcleng@163.com>
2019-09-08 21:27:19 +0800

    first

---
+++ components/heart/index.js
@@ -0,0 +1,38 @@
+// components/heart/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    like: {
+      type: Boolean
+    },
+    count: {
+      type: Number
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    like: true,
+    disLikeSrc: '../images/disLike.png',
+    likeSrc: '../images/like.png',
+    count: 99
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    tapHeart: function () {
+      let {like, count} = this.properties
+      count = like ? count-1 : count+1
+      this.setData({
+        like: !like,
+        count
+      })
+    }
+  }
+})
